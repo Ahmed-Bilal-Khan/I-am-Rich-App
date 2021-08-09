097481f70d3f35a3e808766c8e0bcbbe2a7fc459
ahmed <abk9232@gmail.com>
2021-08-09 17:28:52 +0500

    First Flutter Project

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+// Main is the starting point of all flutter apps
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Text('I am Rich'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('images/diamond.png'),
+        ),
+      ),
+    ),
+  ));
+}
